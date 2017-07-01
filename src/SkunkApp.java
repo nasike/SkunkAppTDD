eec399cdef8a099373610c384d1b01b0db98d80e
nasike <bnasike@gmail.com>
2017-07-01 18:18:14 -0500

    created test for getNumberofPlayers

--- src/SkunkApp.java
+++ src/SkunkApp.java
@@ -1,9 +1,25 @@
 
 public class SkunkApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int numberOfPlayers;
+	private String[] playerNames = new String[2];
 
+	public void getPlayerInfo()
+	{
+		numberOfPlayers = 2;
+		playerNames= new String[numberOfPlayers];
+		
+		playerNames[0] = "Player 1";
+		playerNames[1] = "Player 2";
+		
 	}
-
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	public int getNumberOfPlayers()
+	{
+		return numberOfPlayers;
+	}
 }
--- src/TestSkunkApp.java
+++ src/TestSkunkApp.java
@@ -1,4 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestSkunkApp {
+	private SkunkApp game;
 
+	@Before
+	public void setUp()
+	{
+		game = new SkunkApp();
+}
+	
+	@Test
+	public void testGetPlayerInfo()
+	{
+		game.getPlayerInfo();
+		assertEquals(2,game.getNumberOfPlayers());
+		//assertEquals("Player 1",game.getPlayerName(1));
+		//assertEquals("Player 2",game.getPlayerName(2));
+	}
+	
 }
