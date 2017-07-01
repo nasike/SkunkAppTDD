5ab7756b221eb1a08c70ce7f20eaed68befbe4ae
nasike <bnasike@gmail.com>
2017-07-01 18:35:32 -0500

    created test for Roll die method

--- src/Roll.java
+++ src/Roll.java
@@ -1,4 +1,17 @@
 
 public class Roll {
+	
+private int lastTotal;
+	
+	public Roll()
+	{
+		lastTotal = rollDie() + rollDie();
+	}
 
+	private int rollDie()
+	{
+		return (int) Math.random()*6 + 1;
+	}
+
+	
 }
